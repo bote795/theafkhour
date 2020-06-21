7b0ed25a919a4e123f4a4c68efa1b3a443594fed
Nicolas Botello <nbotell2@gmail.com>
2020-06-21 12:03:09 -0500

    remove unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Container, Row, Col } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
